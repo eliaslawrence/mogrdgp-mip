6aa79a6c13e95a7ac681b13aea48519826dab46c
Elias <eliaslawrence.jr@gmail.com>
2021-01-13 19:42:05 -0300

    pool of solutions

--- mogrdgp1.py
+++ mogrdgp1.py
@@ -10,6 +10,22 @@ def print_matrx(m):
 			print("%-3s" % m[i][j], end=" ")
 		print()
 
+def scatter(ax, pos_clients, pos_stations, pos_prohibited):
+	# plotting location of clients
+	for i, p in enumerate(pos_clients):
+	    ax.scatter((p[0]), (p[1]), marker="o", color="black", s=15)
+	    ax.text((p[0]), (p[1]), "$c_{%d}$" % i)
+
+	# plotting location of stations
+	for i, p in enumerate(pos_stations):
+	    ax.scatter((p[0]), (p[1]), marker="o", color="green", s=15)
+	    ax.text((p[0]), (p[1]), "$s_{%d}$" % i)
+
+	# plotting location of prohibited points
+	for i, p in enumerate(pos_prohibited):
+	    ax.scatter((p[0]), (p[1]), marker="o", color="red", s=15)
+	    ax.text((p[0]), (p[1]), "$p_{%d}$" % i)
+
 clients = gen_clients("instances/eil10.tsp")
 max_x_y = max_pos(clients)
 max_x   = max_x_y[0]
@@ -38,8 +54,8 @@ BI = 100
 DOR = 0.5
 
 # consume
-VEV = 0.1
-FEV = 0.1
+VEV = 0.5
+FEV = 0.5#0.5
 
 # clients matrix
 pos_C = clients#[[6,1],[0,4]]#[[6,1],[0,4],[7,6],[3,8]]#[[x1,y1],[x2,y2]...]
@@ -59,237 +75,236 @@ n, U = 1, set(range(1))
 # max time and list of times
 t_max, T = X_max*Y_max - 1, set(range(X_max*Y_max))
 
-
-# plotting location of clients
-for i, p in enumerate(pos_C):
-    plt.scatter((p[0]), (p[1]), marker="o", color="black", s=15)
-    plt.text((p[0]), (p[1]), "$c_{%d}$" % i)
-
-# plotting location of stations
-for i, p in enumerate(pos_E):
-    plt.scatter((p[0]), (p[1]), marker="o", color="green", s=15)
-    plt.text((p[0]), (p[1]), "$s_{%d}$" % i)
-
-# plotting location of prohibited points
-for i, p in enumerate(pos_P):
-    plt.scatter((p[0]), (p[1]), marker="o", color="red", s=15)
-    plt.text((p[0]), (p[1]), "$p_{%d}$" % i)
+fig, ax = plt.subplots()
+scatter(ax, pos_C, pos_E, pos_P)
 
 #plt.plot((50, 50), (0, X_max))
 
-model = Model()
-
-# binary variables indicating if Client 'c' is visited by UAV 'u' on time 't'
-vC = [[[model.add_var(var_type=BINARY) for t in T] for c in C] for u in U]
-
-# binary variables indicating if Station 'e' is visited by UAV 'u' on time 't'
-vE = [[[model.add_var(var_type=BINARY) for t in T] for e in E] for u in U]
-
-# binary variables indicating if UAV 'u' is not with coord x greater than coord x from Prohibited Point 'p' on time 't'
-uP_geq_x = [[[model.add_var(var_type=BINARY) for t in T] for p in P] for u in U]
-
-# binary variables indicating if UAV 'u' is not with coord x smaller than coord x from Prohibited Point 'p' on time 't'
-uP_leq_x = [[[model.add_var(var_type=BINARY) for t in T] for p in P] for u in U]
-
-# binary variables indicating if UAV 'u' is not with coord y greater than coord x from Prohibited Point 'p' on time 't'
-uP_geq_y = [[[model.add_var(var_type=BINARY) for t in T] for p in P] for u in U]
-
-# binary variables indicating if UAV 'u' is not with coord y smaller than coord x from Prohibited Point 'p' on time 't'
-uP_leq_y = [[[model.add_var(var_type=BINARY) for t in T] for p in P] for u in U]
-
-# binary variables indicating if UAV 'u' is working on time 't'
-on = [[model.add_var(var_type=BINARY) for t in T] for u in U]
-
-# integer variable to show coord x of UAV u on time t
-pos_x = [[model.add_var(var_type=INTEGER) for t in T] for u in U]
-
-# integer variable to show coord y of UAV u on time t
-pos_y = [[model.add_var(var_type=INTEGER) for t in T] for u in U]
-
-# integer variable to show velocity of UAV u on time t
-vel   = [[model.add_var(var_type=INTEGER) for t in T] for u in U]
-
-# integer variable to show recharge rate of UAV u on Station e on time t
-rechargeRate = [[[model.add_var(var_type=INTEGER) for t in T] for e in E] for u in U]
-
-# integer variable to show battery/fuel rate of UAV u on time t
-batRate = [[model.add_var() for t in T] for u in U]
-
-# route duration
-time = model.add_var(var_type=INTEGER)
-
-# UAV charge at the route end
-finalCharge = model.add_var(var_type=INTEGER)
-
-#velInv = [[model.add_var() for t in T] for u in U]
-
-######### OBJECTIVE FUNCTIONS
-
-# time + cons - 5 * finalCharge
-model.objective = minimize(time + xsum(VEV*vel[u][t]/V_max + on[u][t]*FEV for u in U for t in T) - 5 * finalCharge)
-
-######### CONSTRAINTS
-
-# final charge of the route is the minimum final charge among UAVs
-for u in U:
-    model += finalCharge <= batRate[u][t_max]
-
-# route final time
-for u in U:
-	model += time >= xsum(-vel[u][t]/V_max for t in T) + xsum(rechargeRate[u][e][t]*DOR for e in E for t in T)
-#	model += time >= xsum(velInv[u][t] + (on[u][t] - 1) for t in T) + xsum(rechargeRate[u][e][t]*DOR for e in E for t in T)
-
-#for u in U:
-#	for t in T:
-#		model += velInv[u][t] * vel[u][t] == 1
-#		model += velInv[u][t] * (vel[u][t] - on[u][t] + 1) == 1
-
-# initial position
-for u in U:
-    model += pos_x[u][0] == I_x
-    model += pos_y[u][0] == I_y
-
-# adjacent movements only
-for u in U:
-	for t in T:
-		if t >= 1:
-			model += pos_x[u][t] - pos_x[u][t-1] >= -on[u][t]
-			model += pos_x[u][t] - pos_x[u][t-1] <=  on[u][t]
-			model += pos_y[u][t] - pos_y[u][t-1] >= -on[u][t]
-			model += pos_y[u][t] - pos_y[u][t-1] <=  on[u][t]
-
-# grid limits
-for u in U:
-	for t in T:
-		model += pos_x[u][t] >= 0
-		model += pos_x[u][t] <= X_max
-		model += pos_y[u][t] >= 0
-		model += pos_y[u][t] <= Y_max
-
-# client visited
-for u in U:
-	for c in C:
-		for t in T:
-			model +=  pos_x[u][t] - pos_C[c][0] <= X_max*(1 - vC[u][c][t])
-			model += -pos_x[u][t] + pos_C[c][0] <= X_max*(1 - vC[u][c][t])
-			model +=  pos_y[u][t] - pos_C[c][1] <= Y_max*(1 - vC[u][c][t])
-			model += -pos_y[u][t] + pos_C[c][1] <= Y_max*(1 - vC[u][c][t])
-
-# all clients must be visited
-for c in C:
-	model += xsum(vC[u][c][t] for u in U for t in T) >= 1
-
-# prohibited points
-#for u in U:
-#	for p in P:
-#		for t in T:
-#			model +=  pos_x[u][t] - pos_P[p][0] >= 1 - X_max*uP_geq_x[u][p][t]
-#			model += -pos_x[u][t] + pos_P[p][0] >= 1 - X_max*uP_leq_x[u][p][t]
-#			model +=  pos_y[u][t] - pos_P[p][1] >= 1 - Y_max*uP_geq_y[u][p][t]
-#			model += -pos_y[u][t] + pos_P[p][1] >= 1 - Y_max*uP_leq_y[u][p][t]
-#			model += uP_geq_x[u][p][t] + uP_leq_x[u][p][t] + uP_geq_y[u][p][t] + uP_leq_y[u][p][t] <= 3 
-
-# speed limit
-for u in U:
-	for t in T:
-		model += on[u][t]  <= vel[u][t]
-		model += vel[u][t] <= V_max
-
-for u in U:
-	for t in T:
-		if t >= 1:
-			model += on[u][t] <= on[u][t-1]
-
-# initial battery charge
-for u in U:
-	model += batRate[u][0] == BI
-
-# consumption
-for u in U:
-	for t in T:
-		if t >= 1:
-			model += batRate[u][t] == batRate[u][t-1] - VEV * vel[u][t]/V_max - on[u][t]*FEV + xsum(rechargeRate[u][e][t] for e in E)
-
-# battery limits
-for u in U:
-	for t in T:
-		model += batRate[u][t] <= 100
-		model += batRate[u][t] >= 0
-
-# recharging / refueling
-for u in U:
-	for e in E:
-		for t in T:
-			model +=  pos_x[u][t] - pos_E[e][0] <= X_max*(1 - vE[u][e][t])
-			model += -pos_x[u][t] + pos_E[e][0] <= X_max*(1 - vE[u][e][t])
-			model +=  pos_y[u][t] - pos_E[e][1] <= Y_max*(1 - vE[u][e][t])
-			model += -pos_y[u][t] + pos_E[e][1] <= Y_max*(1 - vE[u][e][t])
-			model += rechargeRate[u][e][t] / 100 <= vE[u][e][t] 
-			model += rechargeRate[u][e][t] / 100 <= on[u][t] 
-
-# optimizing
-model.optimize()
-
 plt.savefig("location.pdf")
 
-# checking if a solution was found
-if model.num_solutions:
-	out.write('routes with total cost %g found: ' % (model.objective_value))
+for count in range(1):
+	model = Model()
+
+	# binary variables indicating if Client 'c' is visited by UAV 'u' on time 't'
+	vC = [[[model.add_var(var_type=BINARY) for t in T] for c in C] for u in U]
+
+	# binary variables indicating if Station 'e' is visited by UAV 'u' on time 't'
+	vE = [[[model.add_var(var_type=BINARY) for t in T] for e in E] for u in U]
+
+	# binary variables indicating if UAV 'u' is not with coord x greater than coord x from Prohibited Point 'p' on time 't'
+	uP_geq_x = [[[model.add_var(var_type=BINARY) for t in T] for p in P] for u in U]
+
+	# binary variables indicating if UAV 'u' is not with coord x smaller than coord x from Prohibited Point 'p' on time 't'
+	uP_leq_x = [[[model.add_var(var_type=BINARY) for t in T] for p in P] for u in U]
+
+	# binary variables indicating if UAV 'u' is not with coord y greater than coord x from Prohibited Point 'p' on time 't'
+	uP_geq_y = [[[model.add_var(var_type=BINARY) for t in T] for p in P] for u in U]
+
+	# binary variables indicating if UAV 'u' is not with coord y smaller than coord x from Prohibited Point 'p' on time 't'
+	uP_leq_y = [[[model.add_var(var_type=BINARY) for t in T] for p in P] for u in U]
+
+	# binary variables indicating if UAV 'u' is working on time 't'
+	on = [[model.add_var(var_type=BINARY) for t in T] for u in U]
+
+	# integer variable to show coord x of UAV u on time t
+	pos_x = [[model.add_var(var_type=INTEGER) for t in T] for u in U]
+
+	# integer variable to show coord y of UAV u on time t
+	pos_y = [[model.add_var(var_type=INTEGER) for t in T] for u in U]
+
+	# integer variable to show velocity of UAV u on time t
+	vel   = [[model.add_var(var_type=INTEGER) for t in T] for u in U]
+
+	# integer variable to show recharge rate of UAV u on Station e on time t
+	rechargeRate = [[[model.add_var(var_type=INTEGER) for t in T] for e in E] for u in U]
+
+	# integer variable to show battery/fuel rate of UAV u on time t
+	batRate = [[model.add_var() for t in T] for u in U]
+
+	# route duration
+	time = model.add_var(var_type=INTEGER)
+
+	# UAV charge at the route end
+	finalCharge = model.add_var(var_type=INTEGER)
+
+	#velInv = [[model.add_var() for t in T] for u in U]
+
+	######### OBJECTIVE FUNCTIONS
+	# coeficients
+	a1, a2, a3, a4, a5 = 2, 4, 1, 5*(count+1), 1#2, 4, 1, 20, 1
+
+	# time + cons - 5 * finalCharge
+	#model.objective = minimize((time + xsum((VEV*vel[u][t]/V_max + on[u][t]*FEV)/(VEV+FEV) for u in U for t in T))/t_max)# - finalCharge/100)
+	model.objective = minimize((xsum(-a3*vel[u][t]/V_max + a4*on[u][t] for u in U for t in T) + xsum(a5*rechargeRate[u][e][t]/100 for u in U for e in E for t in T) + a1*xsum((VEV*vel[u][t]/V_max + on[u][t]*FEV)/(VEV+FEV) for u in U for t in T))/t_max - a2*finalCharge/100)
+
+	######### CONSTRAINTS
+
+	# final charge of the route is the minimum final charge among UAVs
 	for u in U:
-		out.write('[%s,%s]' % (pos_x[u][0].x, pos_y[u][0].x))
-#		for t in T:
-#			out.write('%s (%s) ->' % (vel[u][t].x, on[u][t].x))
-#			out.write(' -> [%s,%s]' % (pos_x[u][t].x, pos_y[u][t].x))
-		t = 1
-		plot_x = [pos_x[u][0].x] 
-		plot_y = [pos_y[u][0].x] 
+	    model += finalCharge <= batRate[u][t_max]
 
-		while True:        	
-			if not on[u][t].x:
-				break
-			out.write(' -> [%s,%s]' % (pos_x[u][t].x, pos_y[u][t].x))							    
-			plot_x.append(pos_x[u][t].x)
-			plot_y.append(pos_y[u][t].x)
-			t += 1
-		out.write('\n')	
+	# route final time
+	#for u in U:
+	#	model += time >= xsum(-a3*vel[u][t]/V_max + a4*on[u][t] for t in T) + xsum(a5*rechargeRate[u][e][t]/100 for e in E for t in T)
+	#	model += time >= xsum(velInv[u][t] + (on[u][t] - 1) for t in T) + xsum(rechargeRate[u][e][t]*DOR for e in E for t in T)
 
-		# plotting allocations
-		plt.plot(plot_x, plot_y, color=(255/255,170/255,0))
-		plt.savefig("location-sol.pdf")		
+	#for u in U:
+	#	for t in T:
+	#		model += velInv[u][t] * vel[u][t] == 1
+	#		model += velInv[u][t] * (vel[u][t] - on[u][t] + 1) == 1
 
-		############################
+	# initial position
+	for u in U:
+	    model += pos_x[u][0] == I_x
+	    model += pos_y[u][0] == I_y
 
-		out.write('VEL: %s' % (vel[u][0].x))
-		t = 1
-		while True:        	
-			if not on[u][t].x:
-				break
-			out.write(' -> %s' % (vel[u][t].x))
-			t += 1
-		out.write('\n')
+	# adjacent movements only
+	for u in U:
+		for t in T:
+			if t >= 1:
+				model += pos_x[u][t] - pos_x[u][t-1] >= -on[u][t]
+				model += pos_x[u][t] - pos_x[u][t-1] <=  on[u][t]
+				model += pos_y[u][t] - pos_y[u][t-1] >= -on[u][t]
+				model += pos_y[u][t] - pos_y[u][t-1] <=  on[u][t]
 
-		############################
+	# grid limits
+	for u in U:
+		for t in T:
+			model += pos_x[u][t] >= 0
+			model += pos_x[u][t] <= X_max
+			model += pos_y[u][t] >= 0
+			model += pos_y[u][t] <= Y_max
 
-		out.write('BAT RATE: %s' % (batRate[u][0].x))
-		t = 1
-		while True:        	
-			if not on[u][t].x:
-				break
-			out.write(' -> %s' % (batRate[u][t].x))
-			t += 1
-		out.write('\n')
+	# client visited
+	for u in U:
+		for c in C:
+			for t in T:
+				model +=  pos_x[u][t] - pos_C[c][0] <= X_max*(1 - vC[u][c][t])
+				model += -pos_x[u][t] + pos_C[c][0] <= X_max*(1 - vC[u][c][t])
+				model +=  pos_y[u][t] - pos_C[c][1] <= Y_max*(1 - vC[u][c][t])
+				model += -pos_y[u][t] + pos_C[c][1] <= Y_max*(1 - vC[u][c][t])
 
-		############################
+	# all clients must be visited
+	for c in C:
+		model += xsum(vC[u][c][t] for u in U for t in T) >= 1
 
+	# prohibited points
+	#for u in U:
+	#	for p in P:
+	#		for t in T:
+	#			model +=  pos_x[u][t] - pos_P[p][0] >= 1 - X_max*uP_geq_x[u][p][t]
+	#			model += -pos_x[u][t] + pos_P[p][0] >= 1 - X_max*uP_leq_x[u][p][t]
+	#			model +=  pos_y[u][t] - pos_P[p][1] >= 1 - Y_max*uP_geq_y[u][p][t]
+	#			model += -pos_y[u][t] + pos_P[p][1] >= 1 - Y_max*uP_leq_y[u][p][t]
+	#			model += uP_geq_x[u][p][t] + uP_leq_x[u][p][t] + uP_geq_y[u][p][t] + uP_leq_y[u][p][t] <= 3 
+
+	# speed limit
+	for u in U:
+		for t in T:
+			model += on[u][t]  <= vel[u][t]
+			model += vel[u][t] <= V_max
+
+	for u in U:
+		for t in T:
+			if t >= 1:
+				model += on[u][t] <= on[u][t-1]
+
+	# initial battery charge
+	for u in U:
+		model += batRate[u][0] == BI
+
+	# consumption
+	for u in U:
+		for t in T:
+			if t >= 1:
+				model += batRate[u][t] == batRate[u][t-1] - VEV * vel[u][t]/V_max - on[u][t]*FEV + xsum(rechargeRate[u][e][t] for e in E)
+
+	# battery limits
+	for u in U:
+		for t in T:
+			model += batRate[u][t] <= 100
+			model += batRate[u][t] >= 0
+
+	# recharging / refueling
+	for u in U:
 		for e in E:
-			out.write('RECHARGE RATE: %s' % (rechargeRate[u][e][0].x))
+			for t in T:
+				model +=  pos_x[u][t] - pos_E[e][0] <= X_max*(1 - vE[u][e][t])
+				model += -pos_x[u][t] + pos_E[e][0] <= X_max*(1 - vE[u][e][t])
+				model +=  pos_y[u][t] - pos_E[e][1] <= Y_max*(1 - vE[u][e][t])
+				model += -pos_y[u][t] + pos_E[e][1] <= Y_max*(1 - vE[u][e][t])
+				model += rechargeRate[u][e][t] / 100 <= vE[u][e][t] 
+				model += rechargeRate[u][e][t] / 100 <= on[u][t] 
+
+	# optimizing
+	model.optimize()
+
+	# checking if a solution was found
+	print('num_solutions:', model.num_solutions)
+	for k in range(model.num_solutions):
+		out.write('routes with total cost %g found: ' % (model.objective_values[k]))
+		for u in U:
+			out.write('[%s,%s]' % (pos_x[u][0].xi(k), pos_y[u][0].xi(k)))
+	#		for t in T:
+	#			out.write('%s (%s) ->' % (vel[u][t].x, on[u][t].x))
+	#			out.write(' -> [%s,%s]' % (pos_x[u][t].x, pos_y[u][t].x))
+			t = 1
+			
+			plot_x = [pos_x[u][0].xi(k)] 
+			plot_y = [pos_y[u][0].xi(k)] 			
+
+			while True:        	
+				if not on[u][t].xi(k):
+					break
+				out.write(' -> [%s,%s]' % (pos_x[u][t].xi(k), pos_y[u][t].xi(k)))							    
+				plot_x.append(pos_x[u][t].xi(k))
+				plot_y.append(pos_y[u][t].xi(k))
+				t += 1
+			out.write('\n')	
+
+			# plotting allocations			
+			ax = fig.subplots()
+			ax.plot(plot_x, plot_y, color=(255/255,170/255,0))
+			scatter(ax, pos_C, pos_E, pos_P)
+			plt.savefig("location-sol-%g.pdf" % k)
+			ax.clear()		
+
+			############################
+
+			out.write('VEL: %s' % (vel[u][0].xi(k)))
 			t = 1
 			while True:        	
-				if not on[u][t].x:
+				if not on[u][t].xi(k):
 					break
-				out.write(' -> %s' % (rechargeRate[u][e][t].x))
+				out.write(' -> %s' % (vel[u][t].xi(k)))
 				t += 1
 			out.write('\n')
+
+			############################
+
+			out.write('BAT RATE: %s' % (batRate[u][0].xi(k)))
+			t = 1
+			while True:        	
+				if not on[u][t].xi(k):
+					break
+				out.write(' -> %s' % (batRate[u][t].xi(k)))
+				t += 1
+			out.write('\n')
+
+			############################
+
+			for e in E:
+				out.write('RECHARGE RATE: %s' % (rechargeRate[u][e][0].xi(k)))
+				t = 1
+				while True:        	
+					if not on[u][t].xi(k):
+						break
+					out.write(' -> %s' % (rechargeRate[u][e][t].xi(k)))
+					t += 1
+				out.write('\n')
+
+
+
 
 #print("status =", m.status, "obj =", m.objective_value)
 
